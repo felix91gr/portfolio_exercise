58c2e9d41453eb281793f6664aaba0baeca0d438
Félix Fischer <felix91gr@gmail.com>
2025-06-29 19:01:19 -0400

    Add instructions and notes

---
+++ Portfolio_Exercise.py
@@ -0,0 +1,26 @@
+## Instructions
+# Construct a simple Portfolio class that has
+# - A collection of Stocks.
+#
+# Assume each Stock has
+# - A “Current Price” method that
+#   - Receives the last available price.
+#
+# Also, the Portfolio class has
+# - A collection of “allocated” Stocks that
+#   - Represents the distribution of the Stocks
+#     the Portfolio is aiming (i.e. 40% META, 60% APPL)
+#
+# Provide
+# A portfolio rebalance method, to
+# - Know which Stocks should be sold and
+# - Which ones should be bought,
+# to have a balanced Portfolio; based on the portfolio’s allocation.
+# Add documentation/comments to understand your thinking process and solution
+
+
+## Notes for me, on formatting:
+## https://peps.python.org/pep-0008/#function-and-variable-names
+## Fn names should be lowercase, words separated by underscores.
+## Variable names should follow the same convention
+
